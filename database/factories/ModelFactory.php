6e5a99ffe76d442b78a6ccd8729c39da74c743ac
Ho Thi Thien Trang <ho.thi.thien.trang@framgia.com>
2017-03-18 11:27:56 +0700

    Create seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -10,15 +10,28 @@
 | database. Just tell the factory how a default model should look.
 |
 */
+/** */
+$factory->define(App\Models\Position::class, function (Faker\Generator $faker) {
+    return [
+        'name' 			=> $faker->name,
+        'short_name' 	=> $faker->name,
+    ];
+});
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'name' 			=> $faker->name,
+        'email' 		=> $faker->unique()->safeEmail,
+        'password' 		=> $password ?: $password = bcrypt('123456'),
+        'birthday'		=> $faker->date(),
+        'role'			=> randomDigit,
+        'position_id'	=> function () {
+            	return factory(App\Models\User::class)->create()->id;
+        },
+        'deleted_at'		=> str_random(10),
+        'remember_token' 	=> str_random(10),
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+    	$this->call(PositionsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        // $this->call(ProjectsTableSeeder::class);
+        // $this->call(SkillsTableSeeder::class);
+        // $this->call(TeamsTableSeeder::class);
+        // $this->call(SkillUsersTableSeeder::class
+        // $this->call(TeamUsersTableSeeder::class);
+        // $this->call(PositionTeamsTableSeeder::class);  
+        // $this->call(ProjectTeamsTableSeeder::class);     
+        // $this->call(ActivitiesTableSeeder::class);          
     }
 }
