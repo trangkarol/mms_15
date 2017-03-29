3cc18b9f3f8d3295c78780b323482d0160f00376
Ho Thi Thien Trang <ho.thi.thien.trang@framgia.com>
2017-03-29 15:02:52 +0700

    deploy

---
+++ public/common/js/common.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    // //click on menu
+    // $(document).on('click', '.menu-left-bar li', function() {
+    //     //remove class active
+    //     $('.menu-left-bar li').removeClass('active');
+    //     // add class active
+    //     $(this).addClass('active');
+    // });
+    jQuery(window).resize(function() {
+            jQuery.colorbox.resize({width:"90%"});
+        });
+    //click logout
+    $(document).on('click', '#btn-logout', function() {
+        event.preventDefault();
+        $('#logout-form').submit();
+    });
+});
