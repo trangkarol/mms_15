fa2340f3ec9e7448dd74e8a83827c120a8622079
Ho Thi Thien Trang <ho.thi.thien.trang@framgia.com>
2017-03-30 11:50:05 +0700

    125

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -37,9 +37,9 @@ class ProjectController extends Controller
     public function index()
     {
         // $projects = ProjectTeam::with('teamUser.team', 'teamUser.user', 'project')->where('is_leader', '=', 1)->paginate(15);
-        $projects = $this->project->with(['projectTeams', 'projectTeams.user','projectTeams.teamUser.team','projectTeams.teamUser.user' => function($query) {
+        $projects = $this->project->with(['projectTeams'=> function($query) {
             $query->where('is_leader', '=', 1);
-        }])->paginate(15);
+        }], 'projectTeams.user','projectTeams.teamUser.team','projectTeams.teamUser.user' )->paginate(15);
         dd($projects);
         // $projects = $this->project->with('teamUsers.team', 'teamUsers.user', 'teamUsers')->orderBy('created_at', 'desc')->paginate(15);
         // $projects = TeamUser::with('projects', 'user', 'team')->get();
