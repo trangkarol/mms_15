dfc9f92d238b3b34b0e4370cc98a0271a0446fed
Ho Thi Thien Trang <ho.thi.thien.trang@framgia.com>
2017-03-30 10:02:34 +0700

    116

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -48,14 +48,14 @@ class UserController extends Controller
         $teams = Library::getTeams();
         // $members = TeamUser::with('positions', 'team', 'user.position')->orderBy('created_at', 'desc')->paginate(15);
         $members  = $this->user->leftJoin('team_users', 'users.id', '=', 'team_users.user_id')
-                               ->with('position','teamUsers.team')
+                               ->with('position','teams.team')
                                // ->join('positions as position', 'position.id', '=', 'users.position_id')
                                // ->leftJoin('teams', 'teams.id', '=', 'team_users.team_id')
                                ->leftJoin('position_teams', 'team_users.id', '=', 'position_teams.team_user_id')
-                               ->leftJoin('positions', 'positions.id', '=', 'position_teams.position_id')
+                               // ->leftJoin('positions', 'positions.id', '=', 'position_teams.position_id')
                                // ->select('users.id', 'users.name as userName', 'position.name as positionName', 'teams.name as teamName', 'positions.name as positionTeam')
                                ->get();
-        // dd($members->toArray());
+        dd($members->toArray());
         return view('admin.user.index', compact('members', 'teams'));
     }
 
