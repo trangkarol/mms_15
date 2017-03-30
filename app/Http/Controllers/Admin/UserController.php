796f1b04d34fd0075f48765302ed9d01f3e390f8
Ho Thi Thien Trang <ho.thi.thien.trang@framgia.com>
2017-03-30 10:14:50 +0700

    118

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -53,7 +53,7 @@ class UserController extends Controller
                                ->leftJoin('teams', 'teams.id', '=', 'team_users.team_id')
                                ->leftJoin('position_teams', 'team_users.id', '=', 'position_teams.team_user_id')
                                // ->with('postion_teams.position')
-                               ->join('positions', 'positions.id', '=', 'position_teams.position_id')
+                               ->leftJoin('positions', 'positions.id', '=', 'position_teams.position_id')
                                // ->select('users.id', 'users.name as userName', 'position.name as positionName', 'teams.name as teamName', 'positions.name as positionTeam')
                                ->get();
         dd($members->toArray());
