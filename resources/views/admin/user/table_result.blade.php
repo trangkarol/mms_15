f21aa41fead18262cbc029b9f19acc443e01c4be
Ho Thi Thien Trang <ho.thi.thien.trang@framgia.com>
2017-03-30 10:32:18 +0700

    121

--- resources/views/admin/user/table_result.blade.php
+++ resources/views/admin/user/table_result.blade.php
@@ -12,33 +12,59 @@
     <tbody>
         @if (!empty($members))
             @foreach ($members as $member)
-                <tr>
-                    <td class="text-center">{{ $loop->iteration }}</td>
-                    <td><a href="{{ action('Member\HomeController@detailMember', $member->user->id) }}">{{ $member->user->name }}</a></td>
-                    <td> @if ($member->user->position) {{ $member->user->position->name }} @endif</td>
-                    <!-- <td>  </td>-->
-                    <td> @if ($member->team)  {{ $member->team->name }}  @endif</td>
-                    <td>
-                        @if ($member->positions)
-                            @foreach ($member->positions as $positon)
-                               {{ $positon->name }} @php echo ',' @endphp
-                            @endforeach
-                        @endif
-                    </td>
-                    <td>
-                        <div class="col-md-6">
-                            <a href ="{{ action('Admin\UserController@edit', $member->user->id ) }}" class="btn btn-primary"><i class="fa fa-pencil-square-o"></i></a>
-                        </div>
+                @if ($member->teamUsers)
+                    @foreach ($member->teamUsers as $temUser)
+                        <tr>
+                            <td class="text-center">{{ $loop->iteration }}</td>
+                            <td><a href="{{ action('Member\HomeController@detailMember', $member->user->id) }}">{{ $member->name }}</a></td>
+                            <td> @if ($member->position) {{ $member->position->name }} @endif</td>
+                            <!-- <td>  </td>-->
+                            <td> @if ($temUser->team)  {{ $temUser->team->name }}  @endif</td>
+                            <td>
+                                @if ($temUser->positions)
+                                    @foreach ($temUser->positions as $positon)
+                                       {{ $positon->name }} @php echo ',' @endphp
+                                    @endforeach
+                                @endif
+                            </td>
+                            <td>
+                                <div class="col-md-6">
+                                    <a href ="{{ action('Admin\UserController@edit', $member->user->id ) }}" class="btn btn-primary"><i class="fa fa-pencil-square-o"></i></a>
+                                </div>
 
-                        <div class="col-md-6">
-                            {{ Form::open(['action' => 'Admin\UserController@destroy', 'method' => 'POST', 'id' => 'delete-form-user']) }}
+                                <div class="col-md-6">
+                                    {{ Form::open(['action' => 'Admin\UserController@destroy', 'method' => 'POST', 'id' => 'delete-form-user']) }}
 
-                            {{ Form::hidden('userId',$member->user->id) }}
-                            {!! Form::button(trans('admin.lbl-delete'), ['class' => 'btn btn-primary', 'id' => 'btn-delete', 'type' => 'button']) !!}
-                            {{ Form::close() }}
-                        </div>
-                    </td>
-                </tr>
+                                    {{ Form::hidden('userId',$member->user->id) }}
+                                    {!! Form::button(trans('admin.lbl-delete'), ['class' => 'btn btn-primary', 'id' => 'btn-delete', 'type' => 'button']) !!}
+                                    {{ Form::close() }}
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                @else
+                    <tr>
+                        <td class="text-center">{{ $loop->iteration }}</td>
+                        <td><a href="{{ action('Member\HomeController@detailMember', $member->user->id) }}">{{ $member->name }}</a></td>
+                        <td> @if ($member->position) {{ $member->position->name }} @endif</td>
+                        <!-- <td>  </td>-->
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <div class="col-md-6">
+                                <a href ="{{ action('Admin\UserController@edit', $member->user->id ) }}" class="btn btn-primary"><i class="fa fa-pencil-square-o"></i></a>
+                            </div>
+
+                            <div class="col-md-6">
+                                {{ Form::open(['action' => 'Admin\UserController@destroy', 'method' => 'POST', 'id' => 'delete-form-user']) }}
+
+                                {{ Form::hidden('userId',$member->user->id) }}
+                                {!! Form::button(trans('admin.lbl-delete'), ['class' => 'btn btn-primary', 'id' => 'btn-delete', 'type' => 'button']) !!}
+                                {{ Form::close() }}
+                            </div>
+                        </td>
+                    </tr>
+                @endif
             @endforeach
         @endif
     </tbody>
