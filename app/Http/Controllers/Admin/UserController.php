4b3d1dc9fd668c8616447e3f3bad1adb3c1327fc
Ho Thi Thien Trang <ho.thi.thien.trang@framgia.com>
2017-04-03 12:35:21 +0700

    ff

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -165,7 +165,7 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateUserRequest $request)
+    public function update(Request $request)
     {
         DB::beginTransaction();
 
